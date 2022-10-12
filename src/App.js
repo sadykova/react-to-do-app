7bc2163944b333b39ca51dd7188913da06f5b479
sadykova <almira.sadyk@gmail.com>
2022-10-12 11:26:55 -0400

    Render unordered list using JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+const todoList = [
+  { id: 0, title: "Read book chapters" },
+  { id: 1, title: "Watch class videos" },
+  { id: 2, title: "Complete assignment" },
+  { id: 3, title: "Submit assignment" },
+];
+
 function App() {
   return (
     <div>
       <h1>To-do list</h1>
+      <hr />
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
