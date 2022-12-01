c57745e1beb44b19ec6a0a3c29a0153a7f734817
sadykova <almira.sadyk@gmail.com>
2022-12-01 13:52:59 -0500

    Refactor Input with Label to use Component Composition

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function InputWithLabel(props) {
+function InputWithLabel({ todoTitle, handleTitleChange, children }) {
   return (
     <>
-      <label htmlFor="toDoTitle">{props.label}</label> <br />
+      <label htmlFor="toDoTitle">{children}</label> <br />
       <input
-        value={props.todoTitle}
-        onChange={props.handleTitleChange}
+        value={todoTitle}
+        onChange={handleTitleChange}
         type="text"
         id="toDoTitle"
         name="title"
