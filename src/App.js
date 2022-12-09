4d988e6077359a6bcefb8dba287249ca8bb8253d
sadykova <almira.sadyk@gmail.com>
2022-12-09 11:26:29 -0500

    Add 'Remove' button to List items

--- src/App.js
+++ src/App.js
@@ -19,12 +19,18 @@ function App() {
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
+
+  function removeTodo(id) {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  }
+
   return (
     <>
       <h1>To-do list</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <hr />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
   );
 }
